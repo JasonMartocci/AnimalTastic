e590a65dc2b56a06fa37390aaa571bfc22961558
Jason Martocci <Jason Martocci>
2016-03-08 18:18:24 -0500

    buttons now working with database of informaiton when clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
-
 function buttonFeature(value){
-  alert(value);
+    request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+value, true);
+    request.send('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+value, true);
 };
 
 
@@ -27,14 +27,9 @@ $(document).ready(function() {
       $(addto).after(newInput);
 
 
-
-
-
-
     request.onload = function() {
       if (request.status >= 200 && request.status < 400){
         data = JSON.parse(request.responseText).data.image_url;
-        console.log(data);
         document.getElementById("searchResults").innerHTML = '<center><img src = "'+data+'"  title="GIF via Giphy"></center>';
       } else {
         console.log('reached giphy, but API returned an error');
