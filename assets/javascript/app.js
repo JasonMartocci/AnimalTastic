6081ebedcd2b35cff2cd04c4fca2dbc645c644bd
Jason Martocci <Jason Martocci>
2016-03-09 14:04:04 -0500

    click image displays alert based on status

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,11 +3,10 @@ function buttonFeature(value){
     request.send('GET', 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q='+value, true);
 };
 
-
 document.addEventListener('DOMContentLoaded', function () {
 $(document).ready(function() {
 
-    $('.myButton').click(function(e) {
+    $('.myButton').on('click', function(e) {
       var searchFeature = ($('#formValueId').val());
       request = new XMLHttpRequest;
       request.open('GET', 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q='+searchFeature, true);
@@ -29,13 +28,46 @@ $(document).ready(function() {
       request.onload = function() {
         $("#searchResults").empty();
         for (var i = 0; i <= 10; i++) {
-          data = JSON.parse(request.responseText).data[i].images.fixed_height.url;
-          $("#searchResults").append('<img src = "'+data+'"  title="GIF via Giphy">');
-        }
+          var slug = JSON.parse(request.responseText).data[i].slug;
+          var animated = JSON.parse(request.responseText).data[i].images.fixed_height.url;
+          var still = JSON.parse(request.responseText).data[i].images.fixed_height_still.url;
+          var rating = JSON.parse(request.responseText).data[i].rating;
+
+          $("#searchResults").append('<img src = "'+animated+'"  alt="'+slug+'" class="pause" status="moving"> Rating: '+rating+'');
+        };
+
+        $(document.body).on('click', '.pause', function() {
+
+          var status = $(this).attr('status');
+          var imageUrl = $(this).attr('src');
+
+          console.log(imageUrl);
+         
+          if (status=='still'){
+
+            alert("Still Image");
+
+            $(this).attr('status', 'moving');
+
+            $("#searchResults").append('<img src = "'+animated+'"  alt="'+slug+'" class="pause" status="moving"> Rating: '+rating+'');
+
+          }else if (status=='moving'){
+
+            alert("Moving Image");
+
+            $(this).attr('status', 'still');
+
+            $("#searchResults").append('<img src = "'+still+'"  alt="'+slug+'" class="pause" status="still"> Rating: '+rating+'');
+
+          }
+          return false;
+        });
       };
+
       request.onerror = function() {
         console.log('connection error');
       };
+
       request.send();
     });
   });
